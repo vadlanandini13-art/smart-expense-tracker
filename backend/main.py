a93d92e792e19a6fb64b936faa261b29a7146193
Vadla Nandini <vadlanandini13@gmail.com>
2026-07-11 16:45:06 +0530

    Connected all form inputs to React state

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Allow React to communicate with FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Data model
+class Expense(BaseModel):
+    title: str
+    amount: str
+    category: str
+    date: str
+
+
 @app.get("/")
 def home():
-    return {"message": "Welcome to Smart Expense Tracker API!"}
+    return {"message": "Backend is running!"}
+
+
+@app.post("/expense")
+def add_expense(expense: Expense):
+    print("Expense received:", expense)
+
+    return {
+        "message": "Expense received successfully!",
+        "expense": expense
+    }
--- frontend/src/components/ExpenseForm.jsx
+++ frontend/src/components/ExpenseForm.jsx
@@ -5,11 +5,32 @@ function ExpenseForm() {
   const [category, setCategory] = useState("Food");
   const [date, setDate] = useState("");
 
-  function handleSubmit() {
-    console.log("Title:", title);
-    console.log("Amount:", amount);
-    console.log("Category:", category);
-    console.log("Date:", date);
+  async function handleSubmit() {
+  const expense = {
+    title,
+    amount,
+    category,
+    date,
+  };
+
+  try {
+    const response = await fetch("http://127.0.0.1:8000/expense", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expense),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    alert(data.message);
+  } catch (error) {
+    console.error(error);
+    alert("Something went wrong!");
+  }
 }
  
   return (
@@ -17,17 +38,18 @@ function ExpenseForm() {
       <h2>Add Expense</h2>
 
       <input type="text" placeholder="Expense Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
-      <p>{title}</p>
-      <input type="number" placeholder="Amount" />
+      
 
-      <select>
-        <option>Food</option>
-        <option>Transport</option>
-        <option>Shopping</option>
-        <option>Entertainment</option>
+      <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+
+      <select value={category} onChange={(e) => setCategory(e.target.value) }>
+      <option>Food</option>
+      <option>Transport</option>
+      <option>Shopping</option>
+      <option>Entertainment</option>
       </select>
 
-      <input type="date" />
+      <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
 
       <button onClick={handleSubmit}>Save Expense</button>
     </div>
