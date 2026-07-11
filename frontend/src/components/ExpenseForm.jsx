0ebf02c37be1c2db522d675786c248d9cc6d591e
Vadla Nandini <vadlanandini13@gmail.com>
2026-07-11 15:04:44 +0530

    Added expense form with React state and event handling

--- frontend/src/components/ExpenseForm.jsx
+++ frontend/src/components/ExpenseForm.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 function ExpenseForm() {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("Food");
+  const [date, setDate] = useState("");
+
+  function handleSubmit() {
+    console.log("Title:", title);
+    console.log("Amount:", amount);
+    console.log("Category:", category);
+    console.log("Date:", date);
+}
+ 
   return (
     <div className="expense-form">
       <h2>Add Expense</h2>
 
-      <input type="text" placeholder="Expense Title" />
-
+      <input type="text" placeholder="Expense Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      <p>{title}</p>
       <input type="number" placeholder="Amount" />
 
       <select>
@@ -16,7 +29,7 @@ function ExpenseForm() {
 
       <input type="date" />
 
-      <button>Save Expense</button>
+      <button onClick={handleSubmit}>Save Expense</button>
     </div>
   );
 }
